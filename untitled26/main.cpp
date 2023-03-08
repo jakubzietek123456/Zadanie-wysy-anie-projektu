a8ec3105f1247f61062d05673ed894b4ee4c71ff
jakubzietek123456 <124282075+jakubzietek123456@users.noreply.github.com>
2023-03-08 19:28:07 +0100

    Add files via upload

---
+++ untitled26/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int day;
+
+int main() {
+    cout << "wstaw liczbe: ";
+    cin >> day;
+    switch (day) {
+    case 1:
+        cout << "Monday ";
+    break;
+    case 2:
+        cout << "Tuesday ";
+    break;
+    case 3:
+        cout << "Wendnesday ";
+    break;
+}
+
+    return 0;
+}
